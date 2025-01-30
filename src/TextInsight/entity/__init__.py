7b0aba21cca676491dd9fdbe94170ce439aa41f1
Tanvi <tanvibalsara18@gmail.com>
2025-01-30 20:32:39 +0530

    Data Ingestion file added

--- src/TextInsight/entity/__init__.py
+++ src/TextInsight/entity/__init__.py
@@ -8,3 +8,20 @@ class DataIngestionConfig:
     local_data_file: Path
     unzip_dir: Path
 
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+    unzip_dir: Path
+
+
+
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir: Path
+    STATUS_FILE: str
+    ALL_REQUIRED_FILES: list
