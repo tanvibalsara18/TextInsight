20f217d28ca9287598f08040faf513c026811946
Tanvi <tanvibalsara18@gmail.com>
2025-02-04 15:59:05 +0530

    data transformation added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from TextInsight.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from TextInsight.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
-# from TextInsight.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from TextInsight.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 # from TextInsight.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 # from TextInsight.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 from TextInsight.logging import logger
@@ -29,17 +29,15 @@ except Exception as e:
         logger.exception(e)
         raise e
 
-
-
-# STAGE_NAME = "Data Transformation stage"
-# try:
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-#    data_transformation = DataTransformationTrainingPipeline()
-#    data_transformation.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_transformation = DataTransformationTrainingPipeline()
+   data_transformation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 
